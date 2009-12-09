61cf5a4ab4d7b9e0cb95925acc633aa7cb156d59
Alex Gaynor <alex.gaynor@gmail.com>
2009-12-09 14:04:18 -0500

    Make slug unique and fix the max_length.

--- taggit/models.py
+++ taggit/models.py
@@ -6,7 +6,7 @@ from django.template.defaultfilters import slugify
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, max_length=100)
     
     def __unicode__(self):
         return self.name
