8c443c452ec34463b3a739afbf09de484cbe6670
Alex Gaynor <alex.gaynor@gmail.com>
2009-12-14 22:06:52 -0500

    Added a generic view

---
+++ taggit/views.py
@@ -0,0 +1,15 @@
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import get_object_or_404
+from django.views.generic.object_detail import object_list
+
+from taggit.models import TaggedItem, Tag
+
+
+def tagged_object_list(request, slug, queryset, **kwargs):
+    if callable(queryset):
+        queryset = queryset()
+    tag = get_object_or_404(Tag, slug=slug)
+    qs = queryset.filter(pk__in=TaggedItem.objects.filter(
+        tag=tag, content_type=ContentType.objects.get_for_model(queryset.model)
+    )
+    return object_list(request, queryset, **kwargs)
