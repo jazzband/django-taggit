d932cbb9e41b8f7e9eafcbd772a6afd87124e3cf
Alex Gaynor <alex.gaynor@gmail.com>
2009-12-14 22:07:31 -0500

    small fix

--- taggit/views.py
+++ taggit/views.py
@@ -11,5 +11,5 @@ def tagged_object_list(request, slug, queryset, **kwargs):
     tag = get_object_or_404(Tag, slug=slug)
     qs = queryset.filter(pk__in=TaggedItem.objects.filter(
         tag=tag, content_type=ContentType.objects.get_for_model(queryset.model)
-    )
+    ))
     return object_list(request, queryset, **kwargs)
