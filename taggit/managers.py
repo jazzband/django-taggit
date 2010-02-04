8cbf72802e3d61c8fe1a3dc92e49596e5af53993
Alex Gaynor <alex.gaynor@gmail.com>
2010-02-03 20:14:36 -0500

    Fix other related objects.

--- taggit/managers.py
+++ taggit/managers.py
@@ -184,6 +184,6 @@ def _get_subclasses(model):
     subclasses = [model]
     for f in model._meta.get_all_field_names():
         field = model._meta.get_field_by_name(f)[0]
-        if isinstance(field, RelatedObject) and field.field.rel.parent_link:
+        if isinstance(field, RelatedObject) and getattr(field.field.rel, "parent_link"):
             subclasses.extend(_get_subclasses(field.model))
     return subclasses
