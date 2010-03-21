cd1a857da79c9a614c778ce5cdbc3750c8fa7e10
Alex Gaynor <alex.gaynor@gmail.com>
2010-03-21 13:57:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
+from taggit import VERSION
+
 f = open('README.txt')
 readme = f.read()
 f.close()
 
 setup(
     name='django-taggit',
-    version='0.1-pre',
+    version=".".join(VERSION),
     description='django-taggit is a reusable Django application for simple tagging.',
     long_description=readme,
     author='Alex Gaynor',
@@ -14,7 +16,7 @@ setup(
     url='http://github.com/alex/django-taggit/tree/master',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
