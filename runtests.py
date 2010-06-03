39487e77b1dc9e485d3f892558cf37a0cbb43818
Carl Meyer <carl@oddbird.net>
2010-06-04 04:45:35 +0800

    cleanup

--- runtests.py
+++ runtests.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
-
-from os.path import dirname, abspath
 import sys
 
-from django.conf import settings as django_settings
+from os.path import dirname, abspath
 
-if not django_settings.configured:
-    django_settings.configure(
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure(
         DATABASE_ENGINE='sqlite3',
-        INSTALLED_APPS=(
+        INSTALLED_APPS=[
             'django.contrib.contenttypes',
             'taggit',
             'taggit.tests',
-        )
+        ]
     )
 
+from django.test.simple import run_tests
+    
 def runtests(*test_args):
     if not test_args:
         test_args = ['tests']
     parent = dirname(abspath(__file__))
     sys.path.insert(0, parent)
-    from django.test.simple import run_tests
     failures = run_tests(test_args, verbosity=1, interactive=True)
     sys.exit(failures)
 
