f3a56d573c418b15c6788e95279e632fb3cd199a
Alex Gaynor <alex.gaynor@gmail.com>
2010-08-20 19:03:01 -0500

    Reorder things for easier readability

--- taggit/tests/models.py
+++ taggit/tests/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from taggit.managers import TaggableManager
 from taggit.models import TaggedItemBase
 
+
 class Food(models.Model):
     name = models.CharField(max_length=50)
     
@@ -19,23 +20,23 @@ class Pet(models.Model):
     def __unicode__(self):
         return self.name
 
-
 class HousePet(Pet):
     trained = models.BooleanField()
 
-# test direct-tagging with custom through model
-    
+
+# Test direct-tagging with custom through model
+
 class TaggedFood(TaggedItemBase):
     content_object = models.ForeignKey('DirectFood')
-    
+
+class TaggedPet(TaggedItemBase):
+    content_object = models.ForeignKey('DirectPet')
+
 class DirectFood(models.Model):
     name = models.CharField(max_length=50)
 
     tags = TaggableManager(through=TaggedFood)
 
-class TaggedPet(TaggedItemBase):
-    content_object = models.ForeignKey('DirectPet')
-    
 class DirectPet(models.Model):
     name = models.CharField(max_length=50)
 
@@ -43,15 +44,18 @@ class DirectPet(models.Model):
     
     def __unicode__(self):
         return self.name
-    
 
 class DirectHousePet(DirectPet):
     trained = models.BooleanField()
 
-# test custom through model to model with custom PK
+
+# Test custom through model to model with custom PK
 
 class TaggedCustomPKFood(TaggedItemBase):
     content_object = models.ForeignKey('CustomPKFood')
+
+class TaggedCustomPKPet(TaggedItemBase):
+    content_object = models.ForeignKey('CustomPKPet')
 
 class CustomPKFood(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
@@ -60,10 +64,7 @@ class CustomPKFood(models.Model):
     
     def __unicode__(self):
         return self.name
-    
-class TaggedCustomPKPet(TaggedItemBase):
-    content_object = models.ForeignKey('CustomPKPet')
-    
+
 class CustomPKPet(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
 
