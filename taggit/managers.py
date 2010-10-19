27db151602d38bb6c9a0234cf47d0a9a85ba57cd
Julian Moritz <jmoritz@laptop.(none)>
2010-10-19 17:16:22 +0200

    Added a related_name attribute to TaggableManager.

--- taggit/managers.py
+++ taggit/managers.py
@@ -26,9 +26,9 @@ except NameError:
 
 
 class TaggableRel(ManyToManyRel):
-    def __init__(self, to):
+    def __init__(self, to, related_name=None):
         self.to = to
-        self.related_name = None
+        self.related_name = related_name
         self.limit_choices_to = {}
         self.symmetrical = True
         self.multiple = True
@@ -37,10 +37,10 @@ class TaggableRel(ManyToManyRel):
 
 class TaggableManager(RelatedField):
     def __init__(self, verbose_name=_("Tags"),
-        help_text=_("A comma-separated list of tags."), through=None, blank=False):
+        help_text=_("A comma-separated list of tags."), through=None, blank=False, related_name=None):
         self.use_gfk = through is None or issubclass(through, GenericTaggedItemBase)
         self.through = through or TaggedItem
-        self.rel = TaggableRel(to=self.through._meta.get_field("tag").rel.to)
+        self.rel = TaggableRel(to=self.through._meta.get_field("tag").rel.to, related_name=related_name)
         self.verbose_name = verbose_name
         self.help_text = help_text
         self.blank = blank
@@ -142,10 +142,10 @@ class _TaggableManager(models.Manager):
             name__in=str_tags
         )
         tag_objs.update(existing)
-        
+
         for new_tag in str_tags - set(t.name for t in existing):
             tag_objs.add(self.through.tag_model().objects.create(name=new_tag))
-        
+
         for tag in tag_objs:
             self.through.objects.get_or_create(tag=tag, **self._lookup_kwargs())
 
