7a1a9d00c58f151f584439134285eaf9c402a765
Florian Apolloner <florian@apolloner.eu>
2013-03-25 15:37:06 +0100

    Removed unneeded comments.

--- taggit/views.py
+++ taggit/views.py
@@ -1,7 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404
-#this does not work anymore in django 1.5, see also: https://docs.djangoproject.com/en/1.4/topics/generic-views-migration/
-#from django.views.generic.list_detail import object_list
 from django.views.generic.list import ListView
 
 from taggit.models import TaggedItem, Tag
@@ -17,6 +15,5 @@ def tagged_object_list(request, slug, queryset, **kwargs):
     if "extra_context" not in kwargs:
         kwargs["extra_context"] = {}
     kwargs["extra_context"]["tag"] = tag
-    #return object_list(request, qs, **kwargs)
     return ListView.as_view(request, qs, **kwargs)
 
