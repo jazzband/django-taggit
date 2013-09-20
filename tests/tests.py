jazzband/django-taggit

commit 8c5496f0eafe7d3ec8cb52847de44d3336b18c2d

Fix for django 1.5
from django.views.generic.list_detail import object_list will not work
anymore

taggit/views.py
@@ -1,6 +1,8 @@
 from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404
-from django.views.generic.list_detail import object_list
+#this does not work anymore in django 1.5, see also: https://docs.djangoproject.com/en/1.4/topics/generic-views-migration/
+#from django.views.generic.list_detail import object_list
+from django.views.generic.list import ListView
 
 from taggit.models import TaggedItem, Tag
 
@@ -15,4 +17,6 @@ def tagged_object_list(request, slug, queryset, **kwargs):
     if "extra_context" not in kwargs:
         kwargs["extra_context"] = {}
     kwargs["extra_context"]["tag"] = tag
-    return object_list(request, qs, **kwargs)
+    #return object_list(request, qs, **kwargs)
+    return ListView.as_view(request, qs, **kwargs)
+

commit 124e54e5e4c19623ed17fd49ef894a4fca807f3b

Closes #41: Make tagged items view class-based
The current function-based view is broken since commit
8c5496f0eafe7d3ec8cb52847de44d3336b18c2d.

runtests.py
@@ -17,7 +17,11 @@ if not settings.configured:
             'django.contrib.contenttypes',
             'taggit',
             'tests',
-        ]
+        ],
+        TEMPLATE_LOADERS = (
+            'django.template.loaders.app_directories.Loader',
+        ),
+        ROOT_URLCONF=''
     )
 
 

taggit/views.py
@@ -7,15 +7,24 @@ from django.views.generic.list import ListView
 from taggit.models import TaggedItem, Tag
 
 
-def tagged_object_list(request, slug, queryset, **kwargs):
-    if callable(queryset):
-        queryset = queryset()
-    tag = get_object_or_404(Tag, slug=slug)
-    qs = queryset.filter(pk__in=TaggedItem.objects.filter(
-        tag=tag, content_type=ContentType.objects.get_for_model(queryset.model)
-    ).values_list("object_id", flat=True))
-    if "extra_context" not in kwargs:
-        kwargs["extra_context"] = {}
-    kwargs["extra_context"]["tag"] = tag
-    return ListView.as_view(request, qs, **kwargs)
+class TaggedObjectList(ListView):
+    """Show all objects with a given tag.
 
+    The URLconf has to capture a parameter `slug` or pass it as additional
+    keyword argument.
+    """
+
+    def get_queryset(self):
+        self.tag = get_object_or_404(Tag, slug=self.kwargs['slug'])
+
+        queryset = super(TaggedObjectList, self).get_queryset()
+        queryset = queryset.filter(pk__in=TaggedItem.objects.filter(
+            tag=self.tag, content_type=ContentType.objects.get_for_model(queryset.model)
+        ).values_list("object_id", flat=True))
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = {'tag': self.tag}
+        context.update(kwargs)
+        return super(TaggedObjectList, self).get_context_data(**context)

tests/tests.py
@@ -519,3 +519,23 @@ class TagStringParseTestCase(UnitTestCase):
         self.assertEqual(edit_string_for_tags([plain, spaces, comma]), '"com,ma", "spa ces", plain')
         self.assertEqual(edit_string_for_tags([plain, comma]), '"com,ma", plain')
         self.assertEqual(edit_string_for_tags([comma, spaces]), '"com,ma", "spa ces"')
+
+
+class ViewsTestCase(TestCase):
+    urls = 'tests.urls'
+
+    def setUp(self):
+        self.apple = Food.objects.create(name="apple")
+        self.dog = Pet.objects.create(name="jack")
+        self.tag_yummy = Tag.objects.create(name="yummy")
+        self.apple.tags.add(self.tag_yummy)
+
+    def test_view_returns_tagged_objects(self):
+        response = self.client.get('/tagged/%s/' % self.tag_yummy.slug)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual([o.pk for o in response.context['object_list']], [self.apple.pk])
+        self.assertEqual(response.context['tag'].pk, self.tag_yummy.pk)
+
+    def test_404_for_unknown_tag(self):
+        response = self.client.get('/tagged/unknown-tag/')
+        self.assertEqual(response.status_code, 404)

tests/urls.py
@@ -1,0 +1,9 @@
+from django.conf.urls import patterns
+
+from taggit.views import TaggedObjectList
+from .models import Food
+
+
+urlpatterns = patterns('',
+    (r'^tagged/(?P<slug>[\w+]+)/$', TaggedObjectList.as_view(model=Food)),
+)