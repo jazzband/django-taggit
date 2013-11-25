961ef4ed1d14ed53dcb95fbd293cfb33c8f72665
Florian Apolloner <florian@apolloner.eu>
2013-11-25 23:19:17 +0100

    Release 0.11.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='django-taggit',
-    version='0.11',
+    version='0.11.1',
     description='django-taggit is a reusable Django application for simple tagging.',
     long_description=readme,
     author='Alex Gaynor',
