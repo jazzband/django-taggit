b2c472c3828dc6483d0b0114a6788d854e04d009
Florian Apolloner <florian@apolloner.eu>
2014-04-21 18:39:07 +0200

    Used unicode_literals in migrations.

--- taggit/migrations/0001_initial.py
+++ taggit/migrations/0001_initial.py
@@ -1,4 +1,5 @@
 # encoding: utf8
+from __future__ import unicode_literals
 from django.db import models, migrations
 
 
@@ -12,27 +13,27 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Tag',
             fields=[
-                (u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(unique=True, max_length=100, verbose_name=u'Name')),
-                ('slug', models.SlugField(unique=True, max_length=100, verbose_name=u'Slug')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=100, verbose_name='Name')),
+                ('slug', models.SlugField(unique=True, max_length=100, verbose_name='Slug')),
             ],
             options={
-                u'verbose_name': u'Tag',
-                u'verbose_name_plural': u'Tags',
+                'verbose_name': 'Tag',
+                'verbose_name_plural': 'Tags',
             },
             bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='TaggedItem',
             fields=[
-                (u'id', models.AutoField(verbose_name=u'ID', serialize=False, auto_created=True, primary_key=True)),
-                ('tag', models.ForeignKey(to='taggit.Tag', to_field=u'id')),
-                ('object_id', models.IntegerField(verbose_name=u'Object id', db_index=True)),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType', to_field=u'id', verbose_name=u'Content type')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('tag', models.ForeignKey(to='taggit.Tag', to_field='id')),
+                ('object_id', models.IntegerField(verbose_name='Object id', db_index=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType', to_field='id', verbose_name='Content type')),
             ],
             options={
-                u'verbose_name': u'Tagged Item',
-                u'verbose_name_plural': u'Tagged Items',
+                'verbose_name': 'Tagged Item',
+                'verbose_name_plural': 'Tagged Items',
             },
             bases=(models.Model,),
         ),
