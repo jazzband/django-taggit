510c7f8144229ae5d4fee244e221a867ac3c6923
Victor Felder <victorfelder@gmail.com>
2014-09-15 22:06:43 -0400

    Issue #186 tentative fix

--- taggit/managers.py
+++ taggit/managers.py
@@ -429,7 +429,7 @@ class TaggableManager(RelatedField, Field):
             join2infos = linkfield.get_path_info()
         else:
             join1infos = linkfield.get_reverse_path_info()
-            join2infos = [PathInfo(opts, self.model._meta, [object_id_field], self, True, False)]
+            join2infos = [PathInfo(self.model._meta, opts, [object_id_field], self, True, False)]
         pathinfos.extend(join1infos)
         pathinfos.extend(join2infos)
         return pathinfos
