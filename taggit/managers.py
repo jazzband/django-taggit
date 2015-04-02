d4b2c0ccb60f21114fc3b83ac8e86e0fdf32e4a1
Kaloian Minkov <bluudar@mail.bg>
2015-04-02 05:54:41 +0000

    Fixes #186 and #264 Unknown column app.object_id Django 1.6

--- taggit/managers.py
+++ taggit/managers.py
@@ -431,7 +431,7 @@ class TaggableManager(RelatedField, Field):
             join2infos = linkfield.get_path_info()
         else:
             join1infos = linkfield.get_reverse_path_info()
-            join2infos = [PathInfo(opts, self.model._meta, [object_id_field], self, True, False)]
+            join2infos = [PathInfo(opts, self.model._meta, [from_field], self, True, False)]
         pathinfos.extend(join1infos)
         pathinfos.extend(join2infos)
         return pathinfos
