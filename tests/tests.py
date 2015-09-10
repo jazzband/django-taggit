c0fb2e4bc13397c78bd083673a255df1174b302b
Kaloian Minkov <kaloian.minkov@gmail.com>
2015-09-10 10:56:07 +0300

    Fix missing import for models

--- tests/tests.py
+++ tests/tests.py
@@ -6,7 +6,7 @@ import django
 from django.contrib.contenttypes.models import ContentType
 from django.core import serializers
 from django.core.exceptions import ImproperlyConfigured, ValidationError
-from django.db import connection
+from django.db import connection, models
 from django.test import TestCase, TransactionTestCase
 from django.test.utils import override_settings
 from django.utils.encoding import force_text
