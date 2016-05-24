9376b01c37ae77a8e7cd2a6257c81c0da830e9bd
Breeze.Kay <wangwenpei@nextoa.com>
2016-05-24 15:34:30 +0800

    change import order to solve isort check

--- taggit/apps.py
+++ taggit/apps.py
@@ -1,6 +1,5 @@
-from django.utils.translation import ugettext_lazy as _
 from django.apps import AppConfig as BaseConfig
-
+from django.utils.translation import ugettext_lazy as _
 
 class AppConfig(BaseConfig):
     name = 'taggit'
