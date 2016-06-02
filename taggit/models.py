8273580493ab18d81f134f5792c123ada0e46781
Liam Andrew <landrew@texastribune.org>
2016-06-02 14:59:46 -0500

    Remove unique constraint on tag name

--- taggit/models.py
+++ taggit/models.py
@@ -44,7 +44,7 @@ except AttributeError:
 
 @python_2_unicode_compatible
 class TagBase(models.Model):
-    name = models.CharField(verbose_name=_('Name'), unique=True, max_length=100)
+    name = models.CharField(verbose_name=_('Name'), max_length=100)
     slug = models.SlugField(verbose_name=_('Slug'), unique=True, max_length=100)
 
     def __str__(self):
