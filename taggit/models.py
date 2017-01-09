94b5cc27c6a640bd84cd219ffe59b56b5674f481
Adam Thomas <sortoflikechess@gmail.com>
2017-01-09 17:46:45 +0000

    Add __gt__ and __lt__ overrides for TagBase.
    Fixes an incompatibility with django-modelcluster and, by extension, Wagtail.
    
    Currently untested, just committing the change to 'save' it.

--- taggit/models.py
+++ taggit/models.py
@@ -27,6 +27,12 @@ class TagBase(models.Model):
     def __str__(self):
         return self.name
 
+    def __gt__(self, other):
+        return self.name > other.name
+
+    def __lt__(self, other):
+        return self.name < other.name
+
     class Meta:
         abstract = True
 
