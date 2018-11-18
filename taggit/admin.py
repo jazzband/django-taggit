b3fbeae8f24eb9c8d1aec45754a26c9b85b6405f
Jon Dufresne <jon.dufresne@gmail.com>
2018-11-18 08:28:23 -0800

    Use @admin.register decorator syntax

--- taggit/admin.py
+++ taggit/admin.py
@@ -9,12 +9,10 @@ class TaggedItemInline(admin.StackedInline):
     model = TaggedItem
 
 
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     inlines = [TaggedItemInline]
     list_display = ["name", "slug"]
     ordering = ["name", "slug"]
     search_fields = ["name"]
     prepopulated_fields = {"slug": ["name"]}
-
-
-admin.site.register(Tag, TagAdmin)
