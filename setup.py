8296071b361e242fa83c93201afe71f5ec5d4bb9
Jon Dufresne <jon.dufresne@gmail.com>
2019-10-27 06:45:17 -0700

    Add testing for Django 3.0

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "Framework :: Django :: 1.11",
         "Framework :: Django :: 2.1",
         "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
