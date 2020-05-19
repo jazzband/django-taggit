0e6a96c95abfdf1949d6bafce8658e221d95b318
Raphael Gaschignard <raphael@rtpg.co>
2020-05-19 21:39:40 +0900

    Prepare for the next release

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,9 @@ project = "django-taggit"
 copyright = "Alex Gaynor and individual contributors."
 
 # The short X.Y version.
-version = "1.2"
+version = "1.3"
 # The full version, including alpha/beta/rc tags.
-release = "1.2.0"
+release = "1.3.0"
 
 intersphinx_mapping = {
     "django": (
