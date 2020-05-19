b6fbde932cdd60b4ad272f7b01de355ef833964c
Raphael Gaschignard <raphael@rtpg.co>
2020-05-19 21:39:40 +0900

    Prepare for the next release

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ copyright = "Alex Gaynor and individual contributors."
 # The short X.Y version.
 version = "1.2"
 # The full version, including alpha/beta/rc tags.
-release = "1.2.0"
+release = "1.2.1"
 
 intersphinx_mapping = {
     "django": (
--- taggit/__init__.py
+++ taggit/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (1, 2, 0)
+VERSION = (1, 2, 1)
 __version__ = ".".join(str(i) for i in VERSION)
 
 default_app_config = "taggit.apps.TaggitAppConfig"
