b0beba488a6a1456e272f6bdfb0f33986d270fe5
Raphael Gaschignard <raphael@rtpg.co>
2021-07-05 23:14:53 +0900

    Remove po file liveliness check as mtimes are not preservered

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,8 @@
 import os
 import os.path
-from pathlib import Path
 
 from django.test import TestCase
 from django.utils import translation
-from django.utils.translation.trans_real import all_locale_paths
 
 from taggit.utils import split_strip
 
@@ -28,22 +26,6 @@ class TestLanguages(TestCase):
 
     def get_locale_dir(self):
         return os.path.join(os.path.dirname(__file__), "..", "taggit", "locale")
-
-    def test_po_files_up_to_date(self):
-        locale_dir = self.get_locale_dir()
-        for locale in os.listdir(locale_dir):
-            # confirm there are files for each locale
-            po_path = os.path.join(locale_dir, locale, "LC_MESSAGES", "django.po")
-            if not os.path.exists(po_path):
-                raise AssertionError(f"Missing .po file for the {locale} locale")
-            mo_path = os.path.join(locale_dir, locale, "LC_MESSAGES", "django.mo")
-            if not os.path.exists(mo_path):
-                raise AssertionError(f"missing .mo file for the {locale} locale")
-
-            if Path(po_path).stat().st_mtime > Path(mo_path).stat().st_mtime:
-                raise AssertionError(
-                    f"{po_path} is newer than {mo_path}, did you forget to run compilemessages?"
-                )
 
     def test_language_file_integrity(self):
         locale_dir = self.get_locale_dir()
