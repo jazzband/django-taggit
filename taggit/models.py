558ec17aafc27fdd30af1ed538810284244fea4a
Antoine Wood <antoinewood@gmail.com>
2024-07-17 09:23:47 -0400

    708 - Remove unused import

--- taggit/models.py
+++ taggit/models.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
