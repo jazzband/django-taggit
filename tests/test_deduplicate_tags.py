e0ebafb30ce683c573e94645f3bf9320db4465c0
fazeelghafoor <mfazeelgh@gmail.com>
2024-08-01 23:27:43 +0500

    add managment command deduplicate_tags for removing case_insensitive duplicate tags

---
+++ tests/test_deduplicate_tags.py
@@ -0,0 +1,66 @@
+from io import StringIO
+from django.core.management import call_command
+from django.test import TestCase
+from django.conf import settings
+from taggit.models import Tag, TaggedItem
+from tests.models import Food, HousePet
+
+
+class DeduplicateTagsTests(TestCase):
+    def setUp(self):
+        settings.TAGGIT_CASE_INSENSITIVE = True
+
+        self.tag1 = Tag.objects.create(name="Python")
+        self.tag2 = Tag.objects.create(name="python")
+        self.tag3 = Tag.objects.create(name="PYTHON")
+
+        self.food_item = Food.objects.create(name="Apple")
+        self.pet_item = HousePet.objects.create(name="Fido")
+
+        self.food_item.tags.add(self.tag1)
+        self.pet_item.tags.add(self.tag2)
+
+    def test_deduplicate_tags(self):
+        self.assertEqual(Tag.objects.count(), 3)
+        self.assertEqual(TaggedItem.objects.count(), 2)
+
+        out = StringIO()
+        call_command("deduplicate_tags", stdout=out)
+
+        self.assertEqual(Tag.objects.count(), 1)
+        self.assertEqual(TaggedItem.objects.count(), 2)
+
+        self.assertTrue(Tag.objects.filter(name__iexact="python").exists())
+        self.assertEqual(
+            TaggedItem.objects.filter(tag__name__iexact="python").count(), 2
+        )
+
+        self.assertIn("Tag deduplication complete.", out.getvalue())
+
+    def test_no_duplicates(self):
+        self.assertEqual(Tag.objects.count(), 3)
+        self.assertEqual(TaggedItem.objects.count(), 2)
+
+        out = StringIO()
+        call_command("deduplicate_tags", stdout=out)
+
+        self.assertEqual(Tag.objects.count(), 1)
+        self.assertEqual(TaggedItem.objects.count(), 2)
+
+        self.assertTrue(Tag.objects.filter(name__iexact="python").exists())
+        self.assertEqual(
+            TaggedItem.objects.filter(tag__name__iexact="python").count(), 2
+        )
+
+        self.assertIn("Tag deduplication complete.", out.getvalue())
+
+    def test_taggit_case_insensitive_not_enabled(self):
+        settings.TAGGIT_CASE_INSENSITIVE = False
+
+        out = StringIO()
+        call_command("deduplicate_tags", stdout=out)
+
+        self.assertIn("TAGGIT_CASE_INSENSITIVE is not enabled.", out.getvalue())
+
+        self.assertEqual(Tag.objects.count(), 3)
+        self.assertEqual(TaggedItem.objects.count(), 2)
