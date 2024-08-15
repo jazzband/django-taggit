667fa4b0f080d9e383b25578a95026fec948108d
Miguel Johnson <miguelbjohnson@gmail.com>
2024-08-15 01:18:00 -0400

    adding an admin action to remove orphaned tags

--- taggit/admin.py
+++ taggit/admin.py
@@ -19,7 +19,7 @@ class TagAdmin(admin.ModelAdmin):
     ordering = ["name", "slug"]
     search_fields = ["name"]
     prepopulated_fields = {"slug": ["name"]}
-    actions = ["render_tag_form"]
+    actions = ["render_tag_form", "remove_orphaned_tags_action"]
 
     def get_urls(self):
         urls = super().get_urls()
@@ -84,3 +84,9 @@ class TagAdmin(admin.ModelAdmin):
             "selected_tag_ids": selected_tag_ids,
         }
         return render(request, "admin/taggit/merge_tags_form.html", context)
+
+    @admin.action(description="Remove orphaned tags")
+    def remove_orphaned_tags_action(self, request, queryset):
+        orphaned_tags = Tag.objects.filter(taggit_taggeditem_items=None)
+        count, _ = orphaned_tags.delete()
+        self.message_user(request, f"Successfully removed {count} orphaned tags.", level="success")
